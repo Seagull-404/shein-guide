316e5aa75e6c660b4d7ca759c62334dd4afd2ac9
Seagull-404 <cn13635706133@gmail.com>
2026-03-10 13:32:53 +0800

    Use fixed filenames without hash to prevent duplicate files

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,21 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: '/shein/',
+  build: {
+    rollupOptions: {
+      output: {
+        // 禁用文件名哈希，使用固定名称
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.')
+          const ext = info[info.length - 1]
+          if (/\.(css)$/i.test(assetInfo.name)) {
+            return 'assets/[name][extname]'
+          }
+          return 'assets/[name][extname]'
+        },
+      },
+    },
+  },
 })
